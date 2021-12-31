52aa8c569ddd9184786792b0bb7d6f6c4c191340
HousniEL <elaich.housni@gmail.com>
2021-12-31 14:46:18 +0100

    v0.0.1

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,8 +1,28 @@
 import Service from "./Service.js";
+import crypto from "crypto";
 
 class UserService extends Service {
     constructor(model){
         super(model);
+    }
+    async insert(data){
+        data.password = crypto.pbkdf2Sync(data.password, process.env.SALT, 1000, 64, `sha512`).toString(`hex`); 
+        try {
+            let item = await this.model.create(data);
+            if (item)
+                return {
+                    error: false,
+                    statusCode: 202,
+                    item
+                };
+        } catch (error) {
+            return {
+                error: true,
+                statusCode: 500,
+                message: error.errmsg || "Not able to create item",
+                errors: error.errors
+            };
+        }
     }
 }
 
