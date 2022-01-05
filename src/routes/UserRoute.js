7d711de9c48352fb309a7676af5f01b2ee583bf5
HousniEL <elaich.housni@gmail.com>
2022-01-05 15:12:20 +0100

    v0.0.1

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import UserController from '../controllers/UserController.js';
+import express from "express";
+import UserController from "../controllers/UserController.js";
 //import { JwtMiddleware } from '../middlewares/JWTMiddleware.js';
 
 const router = express.Router();
 
-router.post('/openid', UserController.openid);
-router.post('/signup', UserController.insert);
-router.post('/signin', UserController.signin);
-router.post('/signout', UserController.signout);
-router.put('/:id', UserController.update);
-router.delete('/:id', UserController.delete);
+router.post("/openid", UserController.openid);
+router.post("/signup", UserController.insert);
+router.post("/signin", UserController.signin);
+router.post("/signout", UserController.signout);
+router.put("/:id", UserController.update);
+router.delete("/:id", UserController.delete);
 
 export default router;
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -5,115 +5,119 @@ import crypto from "crypto";
 import { getAccessToken } from "./JWTService.js";
 
 class UserService extends Service {
-    constructor(model){
-        super(model);
-        this.signin = this.signin.bind(this);
-        this.openid = this.openid.bind(this);
-        this.insertToken = this.insertToken.bind(this);
-        this.userTokenService = new UserTokenService( new UserToken().getInstance() );
+  constructor(model) {
+    super(model);
+    this.signin = this.signin.bind(this);
+    this.openid = this.openid.bind(this);
+    this.insertToken = this.insertToken.bind(this);
+    this.userTokenService = new UserTokenService(new UserToken().getInstance());
+  }
+
+  async insertToken(id, token = null) {
+    if (!token) token = getAccessToken(JSON.stringify({ id: id }));
+    await this.userTokenService.insert({
+      userid: id,
+      token: token,
+    });
+    return token;
+  }
+
+  async insert(data, token = null) {
+    if (!token) {
+      data.password = crypto
+        .pbkdf2Sync(data.password, process.env.SALT, 1000, 64, `sha512`)
+        .toString(`hex`);
     }
-    
-    async insertToken(id, token = null){
-        if(!token) token = getAccessToken(JSON.stringify({ id: id }));
-        await this.userTokenService.insert({
-            userid: id,
+    try {
+      let item = await this.model.create(data);
+      if (item) {
+        var token = await this.insertToken(item._id, token);
+        return {
+          error: false,
+          statusCode: 202,
+          response: {
+            user: item,
             token: token,
-        });
-        return token;
-    }
-
-    async insert(data, token = null){
-        if(!token){
-            data.password = crypto.pbkdf2Sync(data.password, process.env.SALT, 1000, 64, `sha512`).toString(`hex`); 
-        }
-        try {
-            let item = await this.model.create(data);
-            if (item){
-                var token = await this.insertToken(item._id, token);
-                return {
-                    error: false,
-                    statusCode: 202,
-                    response: {
-                        user: item,
-                        token: token
-                    }
-                };
-            }
-        } catch (error) {
-            return {
-                error: true,
-                statusCode: 500,
-                message: error.errmsg || "Not able to create item",
-                errors: error.errors
-            };
-        }
-    }
-
-    async signin(data, token = null){
-        var response = await this.model.findOne({ email : data.email }).exec();
-        if (response) {
-            if(!response.account){
-                var token = await this.insertToken(response._id, token);
-                var hash = crypto.pbkdf2Sync(data.password, process.env.SALT, 1000, 64, `sha512`).toString(`hex`);
-                if( hash === response.password ){
-                    return {
-                        error: false,
-                        statusCode: 202,
-                        response : {
-                            user: response,
-                            token: token,
-                        },
-                    }
-                }
-            }
-            if(response.account && token){
-                var token = await this.insertToken(response._id, token);
-                return {
-                    error: false,
-                    statusCode: 202,
-                    response : {
-                        user: response,
-                        token: token,
-                    },
-                }
-            }
-        }
-        return  {
-            error: true,
-            statusCode: 500,
-            message: "Incorrect email or password.",
+          },
         };
+      }
+    } catch (error) {
+      return {
+        error: true,
+        statusCode: 500,
+        message: error.errmsg || "Not able to create item",
+        errors: error.errors,
+      };
     }
+  }
 
-    async signout(data){
-        var response = await this.model.findOne({ _id: data.id });
-        if(response){
-            var token = await this.userTokenService.getAll({ id: response._id });
-            if(!token.error){
-                await this.userTokenService.delete({ _id: token.id });
-                return {
-                    error: false,
-                    statusCode: 202,
-                };
-            }
-            return token;
+  async signin(data, token = null) {
+    var response = await this.model.findOne({ email: data.email }).exec();
+    if (response) {
+      if (!response.account) {
+        var token = await this.insertToken(response._id, token);
+        var hash = crypto
+          .pbkdf2Sync(data.password, process.env.SALT, 1000, 64, `sha512`)
+          .toString(`hex`);
+        if (hash === response.password) {
+          return {
+            error: false,
+            statusCode: 202,
+            response: {
+              user: response,
+              token: token,
+            },
+          };
         }
-        return  {
-            error: true,
-            statusCode: 500,
-            message: "Unknown user.",
+      }
+      if (response.account && token) {
+        var token = await this.insertToken(response._id, token);
+        return {
+          error: false,
+          statusCode: 202,
+          response: {
+            user: response,
+            token: token,
+          },
         };
+      }
     }
+    return {
+      error: true,
+      statusCode: 500,
+      message: "Incorrect email or password.",
+    };
+  }
 
-    async openid(data){
-        var response = await this.model.findOne({ email: data.email });
-        data.password = "";
-        if( response ){
-            return this.signin(data, data.token);
-        } else {
-            return this.insert(data, data.token);
-        }
+  async signout(data) {
+    var response = await this.model.findOne({ _id: data.id });
+    if (response) {
+      var token = await this.userTokenService.getAll({ id: response._id });
+      if (!token.error) {
+        await this.userTokenService.delete({ _id: token.id });
+        return {
+          error: false,
+          statusCode: 202,
+        };
+      }
+      return token;
     }
+    return {
+      error: true,
+      statusCode: 500,
+      message: "Unknown user.",
+    };
+  }
+
+  async openid(data) {
+    var response = await this.model.findOne({ email: data.email });
+    data.password = "";
+    if (response) {
+      return this.signin(data, data.token);
+    } else {
+      return this.insert(data, data.token);
+    }
+  }
 }
 
 export default UserService;
